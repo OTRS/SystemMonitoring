b86f421e91e2f6be1950559203e2d1e5ac941215
Carlos Rodriguez <carlos.rodriguez@otrs.com>
2018-02-02 17:48:57 -0600

    Reverted style.

--- Kernel/System/PostMaster/Filter/SystemMonitoring.pm
+++ Kernel/System/PostMaster/Filter/SystemMonitoring.pm
@@ -254,7 +254,7 @@ sub _MailParse {
     }
     my $Body = $Param{GetParam}->{Body};
 
-    my @BodyLines = split /\n/, $Param{GetParam}->{Body};
+    my @BodyLines = split /\n/, $Body;
 
     # to remember if an element was found before
     my %AlreadyMatched;
